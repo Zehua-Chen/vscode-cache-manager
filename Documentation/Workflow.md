43516bbd7554d77b1333d20096710af89e9bdc53
Zehua-Chen <peterchen06242000@outlook.com>
2019-03-31 02:49:15 -0500

    [refactor] Remove Redundant Public

---
+++ Documentation/Workflow.md
@@ -0,0 +1,12 @@
+# Workflows
+
+## `vscode-storage` Workflow
+
+1. Parse command line arguments;
+2. Retreive a list of workspace folders;
+3. Filter the list based on command line arguments;
+4. Perform necessary actions;
+
+## `VSCodeStorage` Workflow
+
+1. `VSCodeStorage.find()` is called
--- Sources/VSCodeStorage/FileManagerExtension.swift
+++ Sources/VSCodeStorage/FileManagerExtension.swift
@@ -9,7 +9,7 @@ import Foundation
 public extension FileManager {
 
     /// Return the folder where vs code stores its cache
-    public class var vscodeCachePath: String {
+    class var vscodeCachePath: String {
         #if os(macOS)
             return "/Users/\(NSUserName())/Library/Application Support/Code/User"
         #elseif os(Linux)
